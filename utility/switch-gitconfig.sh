23394b0d0f7cebfed7b404959add6e6da98f98a8
ayushpaharia <ayush.paharia.18@gmail.com>
2023-08-18 14:39:05 +0530

    fix: switch ssh keys

--- utility/switch-gitconfig.sh
+++ utility/switch-gitconfig.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # Check if arguments are provided
-if [ $# -lt 2 ]; then
-  echo "Usage: $0 <user-name> <user-email>"
+if [ $# -lt 3 ]; then
+  echo "Usage: $0 <user-name> <user-email> <(default|credilinq)>"
   exit 1
 fi
 
 # Set global Git configurations
 git config --global user.name "$1"
 git config --global user.email "$2"
+
+if [ "$3" == "default" ]; then
+  mv ~/.ssh/default/id* ~/.ssh/
+fi
+
+if [ "$3" == "credilinq" ]; then
+  mv ~/.ssh/credilinq/id* ~/.ssh/
+fi
 
 # Confirm the new configurations
 echo "Git global configurations updated:"
